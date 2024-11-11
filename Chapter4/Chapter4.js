49029ca38fa7227d24123cd3e481e6e253293272
JasmineMace <1037547107@qq.com>
2024-11-12 00:45:13 +0800

    complete the first question of Chapter 4

--- Chapter4/Chapter4.js
+++ Chapter4/Chapter4.js
@@ -1,26 +1,61 @@
-const JOURNAL = require('./journal');
+// const JOURNAL = require('./journal');
 
-require('./journal')
+// require('./journal')
 
-let journal = [];
+// let journal = [];
 
-function addEntry(events, squirrel) {
-     journal.push({events, squirrel});
-}
+// function addEntry(events, squirrel) {
+//      journal.push({events, squirrel});
+// }
 
-addEntry(["work", "touched tree", "pizza", "running", "television"], false);
-addEntry(["work", "ice cream", "cauliflower", "lasasgna", "touched tree"], false);
-addEntry(["work", "cycling", "break", "peanuts", "beer"], false);
+// addEntry(["work", "touched tree", "pizza", "running", "television"], false);
+// addEntry(["work", "ice cream", "cauliflower", "lasasgna", "touched tree"], false);
+// addEntry(["work", "cycling", "break", "peanuts", "beer"], false);
 
-function tableFor(event, journal) {
-    let table = [0, 0, 0, 0];
-    for (let i = 0; i < journal.length; i ++){
-        let entry = journal[i], index = 0;
-        if (entry.events.includes(event)) index += 1;
-        if (entry.squirrel) index += 2;
-        table[index] += 1;
+// function tableFor(event, journal) {
+//     let table = [0, 0, 0, 0];
+//     for (let i = 0; i < journal.length; i ++){
+//         let entry = journal[i], index = 0;
+//         if (entry.events.includes(event)) index += 1;
+//         if (entry.squirrel) index += 2;
+//         table[index] += 1;
+//     }
+//     return table;
+// }
+
+// console.log(tableFor("pizza", JOURNAL));
+
+
+// let JOURNAL = [
+//     { events: ["event1", "event2"] },
+//     { events: ["event3"] },
+//     { events: ["event4", "event5", "event6"] },
+// ];
+
+// for  (let entry of JOURNAL) {
+//     console.log(`${entry.events.length} events.`);
+// }
+
+
+// 1.Find the sum of the number in a range
+function range(start, end, step = start < end ? 1 : -1) {
+    let array = [];
+    if (step > 0) {
+        for (let i = start; i <= end; i += step) array.push(i);
+    } else {
+        for (let i = start; i >= end; i += step) array.push(i);
     }
-    return table;
+    return array;
 }
 
-console.log(tableFor("pizza", JOURNAL));
+function sum(array) {
+    total = 0
+    for (let element of array) {
+       total += element; 
+    }
+    return total;
+}
+
+console.log(sum(range(1, 10)));
+
+console.log(range(10, 3, -1))
