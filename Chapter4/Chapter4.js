e43d6f79255cc408deb548e1a4eb05d4d34bdcd0
JasmineMace <1037547107@qq.com>
2024-11-16 01:09:16 +0800

    resolve a list

--- Chapter4/Chapter4.js
+++ Chapter4/Chapter4.js
@@ -78,12 +78,42 @@ function reverseArrayInPlace(array) {
         [array[left], array[right]] = [array[right], array[left]]; 
         left++;
         right--;
-    };
+    }
     return array;
-};
+}
 
 // console.log(reverseArrayInPlace([1, 3 ,4]))
 
+function arrayToList(array) {
+    let list = null;
+    for (let i = array.length - 1; i >= 0; i--) {
+        list = {value: array[i], rest: list};
+    }
+    return list;
+}
+
+function listToArray(list) {
+    let array = [];
+    for (let node = list; node; node = node.rest) {
+        array.push(node.value);
+    }
+    return array;
+}
+
+function prepend(value, list) {
+    return {value, rest: list};
+}
+
+function nth(list, num) {
+    if (!list) return undefined;
+    else if (num == 0) return list.value;
+    else return nth(list.rest, num - 1);
+}
+
+console.log(arrayToList([1, 2, 3]));
+console.log(listToArray(arrayToList([1, 2, 3])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 1));
 
 module.exports = {
     range,
